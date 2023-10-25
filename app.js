f310a9e07f469abd82566490fe51c11e9ba835df
Tynberli <tynberli@mail.ru>
2023-10-25 11:31:54 +0300

    назвал переменные

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const positionLat = 10;
 const positionLong = 20;
 const addresslat = 20;
 const addresslong = 20;
-
+//
 console.log(Math.sqrt((positionLong - positionLat) * (positionLong - positionLat) + (addresslong - addresslat) * (addresslong - addresslat)));
