85bc99a0d8ec80cebd175e71052cb580902c1770
Tynberli <tynberli@mail.ru>
2023-10-26 20:29:25 +0300

    7-converter

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-let hasLicence = true;
-let age = 21;
-let isDrunk = false;
+function converter(cash = 1000, well, currency) {
+    switch (true) {
+        case currency === "USD":
+        case currency === "EU":
+        case currency === "CNY":
+            return cash / well;
+        default:
+            return null;
+    }
+}
 
-let canDrive = (hasLicence && age >= 18 && !isDrunk);
-console.log(`${canDrive ? 'Может' : 'Не может'} водить машину`);
+console.log(converter(1000, 93.68, "USD"));
+console.log(converter(1000, 98.71, "EU"));
+console.log(converter(1000, 13.07, "CNY"));
+console.log(converter(1000, 113.51, "GBR"));
