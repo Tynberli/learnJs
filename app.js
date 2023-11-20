8bb7278a18bbd535e7c76ee272af656dc1a42474
Tynberli <tynberli@mail.ru>
2023-11-20 22:44:43 +0300

    сократил код

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ const passwordUser = 'qwer1234';
 console.log(passwordUser);
 
 function crypto(password) {
-    let arr = password.split('');
-    let random = arr.reverse();
-    let str = random.join('');
-    return str;
+    return password.split('').reverse().join('');
 }
 
 let cryptoPass = crypto(passwordUser);
@@ -13,13 +10,8 @@ let cryptoPass = crypto(passwordUser);
 console.log(cryptoPass);
 
 function check(cryptoPass, password) {
-
-    let arr2 = cryptoPass.split('');
-    let reverte = arr2.reverse();
-    let str2 = reverte.join('');
-    console.log(str2);
-
-    if (str2 === password) {
+    let arr2 = cryptoPass.split('').reverse().join('');
+    if (arr2 === password) {
         return true;
     } else {
         return false;
