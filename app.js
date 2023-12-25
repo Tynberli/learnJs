ff75f48c84fc6f454ae475c2ae447cc78a0bc69d
Tynberli <tynberli@mail.ru>
2023-12-25 13:10:44 +0300

    спрятал условие конвертации внутри функции

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-function convert(cash, well, currency) {
-    if (currency === "USD" || currency === "EU" || currency === "CNY") {
-        return cash / well;
-    } else {
+function convert(cash, currency) {
+    if (currency === "USD") {
+        return cash / 93.68;
+    }
+    else if (currency === "EU") {
+        return cash / 98.71;
+    }
+
+    else if (currency === "CNY") {
+        return cash / 13.07;
+    }
+    else{
         return null;
     }
+
 }
 
-const operations = (cash = 1000, well, currency) => dollar = convert(cash, well, currency);
+const operations = (cash, currency) => dollar = convert(cash, currency);
 
-console.log(operations(1000, 93.68, "USD"));
-console.log(operations(1000, 98.71, "EU"));
-console.log(operations(1000, 13.07, "CNY"));
-console.log(operations(1000, 113.51, "GBR"));
+console.log(operations(1000, "USD"));
+console.log(operations(1000, "EU"));
+console.log(operations(1000, "CNY"));
+console.log(operations(1000, "GBR"));
