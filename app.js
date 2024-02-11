dc860910f15e359e33f2df65dabe19383e35e6c6
Tynberli <tynberli@mail.ru>
2024-02-11 23:52:59 +0300

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let hasLicence = true;
 let age = 21;
 let isDrunk = false;
-
+/* ычвф */
 let canDrive = (hasLicence && age >= 18 && !isDrunk);
 console.log(`${canDrive ? 'Может' : 'Не может'} водить машину`);
